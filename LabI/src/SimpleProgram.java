7c65511cdb7d1176ab277456c50b83ef4c9afbad
Sandy <sandyluimo@hotmail.com>
2023-06-25 08:41:45 -0300

    Upload 2

---
+++ LabI/src/SimpleProgram.java
@@ -0,0 +1,9 @@
+
+public class SimpleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Oi Mamãe?");
+	}
+
+}
---
+++ operacaoBasica/src/operacaoBasica/maisMenosEtc.java
@@ -0,0 +1,8 @@
+package operacaoBasica;
+
+public class maisMenosEtc {
+	short x = 6;
+	int y = 4;
+	float a = 12.5f;
+	float b = 7f;
+}
